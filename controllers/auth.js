cf9e5afaa73b2b4de649f4cab00a3b32a3715ffe
darshanraut123 <darshanraut3828@gmail.com>
2022-03-28 01:24:17 +0530

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,287 @@
+const User = require('../modules/user');
+const jwt = require('jsonwebtoken'); //to generate signed token
+const expressJwt = require('express-jwt');
+const _ = require('lodash');
+const { errorHandler } = require("../helpers/dbErrorHandler");
+const { OAuth2Client } = require('google-auth-library');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+//using async/await
+exports.signup = async(req, res) => {
+    try {
+        const user = await new User(req.body);
+        await user.save((err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Email is taken'
+                });
+            }
+            user.salt = undefined;
+            user.hashed_password = undefined;
+            res.status(200).json({ user });
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
+};
+
+
+exports.signin = (req, res) => {
+    // find the user based on email
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist. Please signup'
+            });
+        }
+        // if user is found making sure that the email and password match
+        // create authenticate method in user model
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password dont match'
+            });
+        }
+        // generate a signed token with user id and secret
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        // persist the token as 't' in cookie with expiry date
+        res.cookie('t', token, { expire: new Date() + 9999 });
+        // return response with user and token to frontend client
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, email, name, role } });
+    });
+};
+
+
+
+exports.forgotPassword = (req, res) => {
+    const { email } = req.body;
+
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with that email does not exist'
+            });
+        }
+
+        const token = jwt.sign({ _id: user._id, name: user.name }, process.env.JWT_RESET_PASSWORD, {
+            expiresIn: '10m'
+        });
+
+        // create reusable transporter object using the default SMTP transport
+        const transporter = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+
+
+        return user.updateOne({ resetPasswordLink: token }, (err, success) => {
+            if (err) {
+                console.log('RESET PASSWORD LINK ERROR', err);
+                return res.status(400).json({
+                    error: 'Database connection error on user password forgot request'
+                });
+            } 
+            else {
+
+                // Email the user a unique verification link
+                transporter.sendMail({
+                    to: email,
+                    subject: "Verify Account",
+                    html: `<h1>Please use the following link to reset your password</h1>
+                    <p>${process.env.CLIENT_URL}/auth/password/reset/${token}</p>
+                    <hr />
+                    <p>This email may contain sensetive information</p>
+                    <p>${process.env.CLIENT_URL}</p>`,
+                })
+                .then(sent => {
+                    // console.log('SIGNUP EMAIL SENT', sent)
+                    return res.json({
+                        message: `Email has been sent to ${email}. Follow the instruction to activate your account`
+                    });
+                })
+                .catch(err => {
+                        return res.json({
+                            message: err.message
+                        });
+                    });
+            }
+        });
+    });
+};
+
+exports.resetPassword = (req, res) => {
+    const { resetPasswordLink, newPassword } = req.body;
+
+    if (resetPasswordLink) {
+        jwt.verify(resetPasswordLink, process.env.JWT_RESET_PASSWORD, function(err, decoded) {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Expired link. Try again'
+                });
+            }
+
+            User.findOne({ resetPasswordLink }, (err, user) => {
+                if (err || !user) {
+                    return res.status(400).json({
+                        error: 'Something went wrong. Try later'
+                    });
+                }
+
+                const updatedFields = {
+                    password: newPassword,
+                    resetPasswordLink: ''
+                };
+
+                user = _.extend(user, updatedFields);
+
+                user.save((err, result) => {
+                    if (err) {
+                        return res.status(400).json({
+                            error: 'Error resetting user password'
+                        });
+                    }
+                    res.json({
+                        message: `Great! Now you can login with your new password`
+                    });
+                });
+            });
+        });
+    }
+};
+
+
+
+exports.signout = (req, res) => {
+    res.clearCookie('t');
+    res.json({ message: 'Signout success' });
+};
+
+
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256'],
+    userProperty: 'auth',
+});
+
+//Authenicated User
+exports.isAuth = (req, res, next) => {
+    //auth ->required for reqSignin
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!user) {
+        return res.status(403).json({
+            error: 'Access denied'
+        });
+    }
+    next();
+};
+
+
+//Authenicated Admin
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: 'Admin resourse! Access denied'
+        });
+    }
+    next();
+};
+
+
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+exports.googleLogin = (req, res) => {
+    const { idToken } = req.body;
+
+    client.verifyIdToken({ idToken, audience: process.env.GOOGLE_CLIENT_ID }).then(response => {
+        const { email_verified, name, email } = response.payload;
+        if (email_verified) {
+            User.findOne({ email }).exec((err, user) => {
+                if (user) {
+                    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '2d' });
+                    const { _id, email, name, role } = user;
+                    return res.json({
+                        token,
+                        user: { _id, email, name, role }
+                    });
+                } else {
+                    let password = email + process.env.JWT_SECRET;
+                    user = new User({ name, email, password });
+                    user.save((err, data) => {
+                        if (err) {
+                            console.log('ERROR GOOGLE LOGIN ON USER SAVE', err);
+                            return res.status(400).json({
+                                error: 'User signup failed with google'
+                            });
+                        }
+                        const token = jwt.sign({ _id: data._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                        const { _id, email, name, role } = data;
+                        return res.json({
+                            token,
+                            user: { _id, email, name, role }
+                        });
+                    });
+                }
+            });
+        } else {
+            return res.status(400).json({
+                error: 'Google login failed. Try again'
+            });
+        }
+    });
+};
+
+exports.facebookLogin = (req, res) => {
+    console.log('FACEBOOK LOGIN REQ BODY', req.body);
+    const { userID, accessToken } = req.body;
+
+    const url = `https://graph.facebook.com/v2.11/${userID}/?fields=id,name,email&access_token=${accessToken}`;
+
+    return (
+        fetch(url, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        // .then(response => console.log(response))
+        .then(response => {
+            const { email, name } = response;
+            User.findOne({ email }).exec((err, user) => {
+                if (user) {
+                    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                    const { _id, email, name, role } = user;
+                    return res.json({
+                        token,
+                        user: { _id, email, name, role }
+                    });
+                } else {
+                    let password = email + process.env.JWT_SECRET;
+                    user = new User({ name, email, password });
+                    user.save((err, data) => {
+                        if (err) {
+                            console.log('ERROR FACEBOOK LOGIN ON USER SAVE', err);
+                            return res.status(400).json({
+                                error: 'User signup failed with facebook'
+                            });
+                        }
+                        const token = jwt.sign({ _id: data._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                        const { _id, email, name, role } = data;
+                        return res.json({
+                            token,
+                            user: { _id, email, name, role }
+                        });
+                    });
+                }
+            });
+        })
+        .catch(error => {
+            res.json({
+                error: 'Facebook login failed. Try later'
+            });
+        })
+    );
+};
---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const {
+    signup,
+    signin,
+    forgotPassword,
+    resetPassword,
+    signout,
+    googleLogin,
+    facebookLogin
+} = require('../controllers/auth');
+
+// import validators
+const {
+    userSignupValidator,
+    forgotPasswordValidator,
+    resetPasswordValidator
+} = require('../validator/auth');
+const { runValidation } = require('../validator');
+
+
+
+router.post("/signup", userSignupValidator,runValidation, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
+// forgot reset password
+router.put("/forgot-password", forgotPasswordValidator, runValidation, forgotPassword);
+router.put('/reset-password', resetPasswordValidator, runValidation, resetPassword);
+
+// google and facebook
+router.post('/google-login', googleLogin);
+router.post('/facebook-login', facebookLogin);
+
+
+
+
+module.exports = router;
